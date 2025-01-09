6aa1c1e0b421a077f252c5c5f527df5bd22f80bc
Austin Phillipps <austinphillipps@hotmail.com>
2025-01-09 10:08:18 -0400

    dffdfg

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,36 +1,42 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 
+const getCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const currentUser = await User.findOne({ _id: req.userId });
+    if (!currentUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(currentUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
 
 const createCurrentUser = async (req: Request, res: Response) => {
   try {
-    const data = req.body;
+    const { auth0Id } = req.body;
+    const existingUser = await User.findOne({ auth0Id });
 
-    // Validation simple des données (exemple)
-    if (!data || Object.keys(data).length === 0) {
-      throw new Error('Aucune donnée envoyée dans la requête.');
+    if (existingUser) {
+      return res.status(200).send();
     }
 
-    console.log('Données reçues :', data);
+    const newUser = new User(req.body);
+    await newUser.save();
 
-    // Réponse au client
-    res.status(200).json({
-      message: 'Données reçues avec succès!',
-      receivedData: data,
-    });
+    res.status(201).json(newUser.toObject());
   } catch (error) {
-    console.error('Erreur dans postData :', error.message);
-
-    // Réponse en cas d'erreur
-    res.status(500).json({
-      message: 'Une erreur est survenue.',
-      error: error.message,
-    });
+    console.log(error);
+    res.status(500).json({ message: "Error creating user" });
   }
 };
 
 
 
 export default {
-  createCurrentUser
+  createCurrentUser,
+
 };
